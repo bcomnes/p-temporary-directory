163693902336a2d2ed580ae7b69b6275955f7177
Bret Comnes <bcomnes@gmail.com>
2023-07-31 17:18:12 -0700

    Remove dependencies and add release automation

--- cb.js
+++ cb.js
@@ -1,7 +1,6 @@
-const path = require('path')
-const mkdirp = require('mkdirp')
-const rimraf = require('rimraf')
-const os = require('os')
+const path = require('node:path')
+const fs = require('node:fs')
+const os = require('node:os')
 
 module.exports = function tmpdir (title, cb) {
   if (typeof title === 'function') {
@@ -10,12 +9,12 @@ module.exports = function tmpdir (title, cb) {
   }
 
   var tmp = path.join(os.tmpdir(), title + '-' + Math.random().toString(36).slice(2))
-  mkdirp(tmp, function (err) {
+  fs.mkdir(tmp, { recursive: true }, function (err) {
     if (err) return cb(err)
     cb(null, tmp, cleanup)
 
     function cleanup (cb) {
-      rimraf(tmp, function (err) {
+      fs.rm(tmp, { recursive: true, force: true }, function (err) {
         if (!cb) return
         cb(err)
       })
--- test.js
+++ test.js
@@ -1,39 +1,39 @@
-const tape = require('tape')
-const ptape = require('tape-promise').default
-const test = ptape(tape)
-const fs = require('fs')
+const test = require('node:test')
+const assert = require('node:assert')
+const fs = require('node:fs')
 const pTmp = require('.')
 const tmp = require('./cb')
 
-tape('creates and cleans up tmpdir', function (t) {
+test('creates and cleans up tmpdir', function (t, done) {
   tmp(function (err, tmpdir, cleanup) {
-    t.notOk(err, 'no error')
-    t.ok(fs.existsSync(tmpdir), 'tmpdir exists')
+    assert.ifError(err)
+    assert(fs.existsSync(tmpdir), 'tmpdir exists')
     cleanup(function (err) {
-      t.notOk(err, 'no error')
-      t.notOk(fs.existsSync(tmpdir), 'tmpdir got removed')
-      t.end()
+      assert.ifError(err)
+      assert(!fs.existsSync(tmpdir), 'tmpdir got removed')
+      done()
     })
   })
 })
 
-tape('creates and cleans up tmpdir w/ custom prefix', function (t) {
+test('creates and cleans up tmpdir w/ custom prefix', function (t, done) {
   tmp('cats', function (err, tmpdir, cleanup) {
-    t.notOk(err, 'no error')
-    t.ok(tmpdir.indexOf('cats-') > -1, 'has cats-')
-    t.ok(fs.existsSync(tmpdir), 'tmpdir exists')
+    assert.ifError(err)
+    assert.ok(tmpdir.indexOf('cats-') > -1, 'has cats-')
+    assert.ok(fs.existsSync(tmpdir), 'tmpdir exists')
     cleanup(function (err) {
-      t.notOk(err, 'no error')
-      t.notOk(fs.existsSync(tmpdir), 'tmpdir got removed')
-      t.end()
+      assert.ifError(err)
+      assert.ok(!fs.existsSync(tmpdir), 'tmpdir got removed')
+      done()
     })
   })
 })
 
 test('p-temporary-directory', async t => {
   const [dir, cleanup] = await pTmp()
-  t.equal(typeof dir, 'string', 'dir is a string')
-  t.equal(typeof cleanup, 'function', 'cleanup is a function')
+  assert.equal(typeof dir, 'string', 'dir is a string')
+  assert.equal(typeof cleanup, 'function', 'cleanup is a function')
   const cleanupPromise = cleanup()
-  t.true(cleanupPromise instanceof Promise, 'cleanup returns a promise')
+  assert(cleanupPromise instanceof Promise, 'cleanup returns a promise')
+  await cleanupPromise
 })
